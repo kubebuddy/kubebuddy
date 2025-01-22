e096deb4a5e235e9fda542951cdc94491cef179c
ThinknyxDheeraj <thinknyxdheeraj@MacBookAir.lan>
2025-01-22 11:25:16 +0530

    error page updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,13 +3,42 @@ from main.models import KubeConfig
 from .src import k8s_pods, k8s_nodes
 from django.contrib.auth.decorators import login_required
 
+from kubernetes import config, client
+
+
+
 # Create your views here.
 @login_required
 def dashboard(request):
-    print(KubeConfig.objects.all())
+    cluster_id = 'cluster_id_01' #for reference now, we have to change it to dynamic in future 
+    data = KubeConfig.objects.filter(cluster_id=cluster_id).values().first()
+    path = data['path']
+    print(path)
+
+    # try:
+    config.load_kube_config(config_file=path)  # Load the kube config
+    v1 = client.CoreV1Api()
+    namespaces = v1.list_namespace()
     return render(request, 'dashboard/dashboard.html')
+    # cluster_id = 'cluster_id_01' #for reference now, we have to change it to dynamic in future 
+    # data = KubeConfig.objects.filter(cluster_id=cluster_id).values().first()
+    # path = data['path']
+
+
+    # try:
+    #     config.load_kube_config(config_file=path)  # Load the kube config
+    #     v1 = client.CoreV1Api()
+    #     namespaces = v1.list_namespace()
+    #     return render(request, 'dashboard/dashboard.html')
+    
+    # except Exception as e:
+    #     error_message = f"Error: Unable to connect to the cluster. \n Details: {str(e)}"
+    #     return render(request, 'dashboard/error.html', {"error": error_message})
 
 def pods(request):
+
+
+
     pods, pc = k8s_pods.getpods()
     return render(request, 'dashboard/pods.html', { "pods": pods, "pc": pc})
 

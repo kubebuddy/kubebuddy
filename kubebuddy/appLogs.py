8bae31cd9e4122ac4cff45f798e30e4636c2f5c7
unknown <pulkit.khandelwal@thinknyx.com>
2025-01-22 16:46:35 +0530

    implemented time rotating file handler instead of file handler

--- kubebuddy/appLogs.py
+++ kubebuddy/appLogs.py
@@ -3,23 +3,34 @@ import logging
 from pathlib import Path
 import datetime
 
-# Defining directory where logs are to be stored.
-LOGGING_DIR = os.path.join(os.path.dirname(Path(__file__).resolve().parent),'logs')
-# Defining Log file name
-DATE_TIME = "KB_" + str(datetime.date.today().strftime("%d-%m-%Y")) + '.log'
+# Defining directory where logs are to be stored
 # Defining complete path to Log file
-LOG_FILE_PATH = os.path.join(LOGGING_DIR,DATE_TIME)
+# Defining Log file name
+LOGGING_DIR = os.path.join(os.path.dirname(Path(__file__).resolve().parent),'logs')
+FILE_NAME = "KubeBuddy.log"
+# {DATE}_KubeBuddy.log
+LOG_FILE_PATH = os.path.join(LOGGING_DIR,FILE_NAME)
 
-# Defining file handler to save log data and set level to DEBUG
-file_handler = logging.FileHandler(LOG_FILE_PATH)
-file_handler.setLevel(logging.DEBUG)
+# # Defining file handler to save log data and set level to DEBUG
+# file_handler = logging.FileHandler(LOG_FILE_PATH)
+# file_handler.setLevel(logging.DEBUG)
+
+timed_rotating_file_handler = logging.handlers.TimedRotatingFileHandler(
+    LOG_FILE_PATH,
+    when='S',
+    interval = 5,
+    backupCount=0,
+    encoding='utf8',
+)
+
+timed_rotating_file_handler.setLevel(logging.DEBUG)
 
 # Defining format of log data and setting it to file_handler
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-file_handler.setFormatter(formatter)
+timed_rotating_file_handler.setFormatter(formatter)
 
 # Defining logger, setting level and adding file handler
 logger = logging.getLogger('app_logger')
 logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
+logger.addHandler(timed_rotating_file_handler)

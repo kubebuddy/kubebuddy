bbff5886b32239cfc126c47dcbfc0566f56e79c0
unknown <pulkit.khandelwal@thinknyx.com>
2025-01-23 09:43:35 +0530

    optimized appLogs.py file

--- kubebuddy/appLogs.py
+++ kubebuddy/appLogs.py
@@ -1,22 +1,21 @@
-import os
 import logging
 from pathlib import Path
-import datetime
 
 # Defining directory where logs are to be stored
 # Defining complete path to Log file
 # Defining Log file name
-LOGGING_DIR = os.path.join(os.path.dirname(Path(__file__).resolve().parent),'logs')
+LOGGING_DIR = Path(__file__).resolve().parent / 'logs'
 FILE_NAME = "KubeBuddy.log"
-# {DATE}_KubeBuddy.log
-LOG_FILE_PATH = os.path.join(LOGGING_DIR,FILE_NAME)
+LOG_FILE_PATH = LOGGING_DIR / FILE_NAME
+
+# Creating log directory
+LOGGING_DIR.mkdir(exist_ok=True)
 
 # Defining file handler to save log data and set level to DEBUG
-
 timed_rotating_file_handler = logging.handlers.TimedRotatingFileHandler(
     LOG_FILE_PATH,
-    when='S',
-    interval = 5,
+    when='midnight',
+    interval = 1,
     backupCount=0,
     encoding='utf8',
 )
@@ -25,12 +24,14 @@ timed_rotating_file_handler = logging.handlers.TimedRotatingFileHandler(
 def namer(name):
     return name.replace(".log", "") + ".log"
 
+# Setting logging level to DEBUG, defining suffix for file name change and 
+# replacing namer method of timed_rotating_file_handler to custom one
 timed_rotating_file_handler.setLevel(logging.DEBUG)
 timed_rotating_file_handler.suffix = "%Y-%m-%d"
 timed_rotating_file_handler.namer = namer
+
 # Defining format of log data and setting it to file_handler
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
 timed_rotating_file_handler.setFormatter(formatter)
 
 # Defining logger, setting level and adding file handler

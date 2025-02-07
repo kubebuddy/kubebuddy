58ee3bcae6d6c60ff51770cf6d301f93b7a026be
dheeraj-sain <dheeraj.sain@thinknyx.com>
2025-02-07 14:49:01 +0530

    KBP-26: Dashboard Updated and Pods page.

--- dashboard/src/k8s_namespaces.py
+++ dashboard/src/k8s_namespaces.py
@@ -1,11 +1,13 @@
 from kubernetes import client, config
 config.load_kube_config()
 v1 = client.CoreV1Api()
+
 def get_namespace():
     list_ns = []
     namespaces = v1.list_namespace()
     for ns in namespaces.items:
         list_ns.append(ns.metadata.name)
     return list_ns
+
 n = get_namespace()
 print(n)

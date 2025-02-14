5cdc0475a9b2fd28b0701babcbd9310de4ffb6de
kanishk-thinknyx <kanishk.kumar@thinknyx.com>
2025-02-14 12:01:56 +0530

    KBP-36: Refined code (deleted and alligned unnecessary files and code)

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from main.models import KubeConfig, Cluster
 from .src import k8s_pods, k8s_nodes, k8s_deployments, k8s_daemonset, k8s_replicaset, \
-                k8s_statefulset, k8s_jobs, k8s_cronjobs, k8s_namespaces, k8s_cluster_metric, k8s_events, k8s_rs_basic_info, k8s_rs_management, \
-                k8s_rs_status, k8s_deployments
+                k8s_statefulset, k8s_jobs, k8s_cronjobs, k8s_namespaces, k8s_cluster_metric, k8s_events, \
+                k8s_deployments
 from django.contrib.auth.decorators import login_required
 from kubebuddy.appLogs import logger
 from kubernetes import config, client

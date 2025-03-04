e850664e1aae48dc64a17afa683a0a45e7a390b5
Pulkit-thinknyx <pulkit.khandelwal@thinknyx.com>
2025-03-04 09:11:05 +0530

    KBP-44: Fixed None taints bug

--- dashboard/src/cluster_management/k8s_nodes.py
+++ dashboard/src/cluster_management/k8s_nodes.py
@@ -78,7 +78,7 @@ def get_nodes_info(path: str, context: str):
             "os_image": node.status.node_info.os_image if node.status.node_info.os_image else "-",
             "kernel_version": node.status.node_info.kernel_version if node.status.node_info.kernel_version else "-",
             "container_runtime": node.status.node_info.container_runtime_version if node.status.node_info.container_runtime_version else "-",
-            "taints": [taint.key +"=" + taint.value + ":" + taint.effect for taint in node.spec.taints]
+            "taints": [taint.key +"=" + taint.value + ":" + taint.effect for taint in node.spec.taints if node.spec.taints]
         }
         node_data.append(node_info)
     
@@ -99,7 +99,7 @@ def get_node_description(path=None, context=None, node_name=None):
             "labels": list(node.metadata.labels.items()) if node.metadata.labels else [],
             "annotations": list(node.metadata.annotations.items()) if node.metadata.annotations else [],
             "creation_timestamp": node.metadata.creation_timestamp,
-            "taints": node.spec.taints,
+            "taints": [taint.key +"=" + taint.value + ":" + taint.effect for taint in node.spec.taints if node.spec.taints],
             "unschedulable": node.spec.unschedulable,
             "addresses": {address.type: address.address for address in node.status.addresses} if node.status.addresses else {},
             "capacity": {key: str(value) for key, value in node.status.capacity.items()} if node.status.capacity else {},

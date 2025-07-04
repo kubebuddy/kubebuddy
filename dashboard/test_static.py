c7532ec883009f63d3969c0145cfc47dfebae330
Shon-Kalangutkar <shon.kalangutkar@thinknyx.com>
2025-07-04 16:58:11 +0530

    Made changes in test_static file

--- dashboard/test_static.py
+++ dashboard/test_static.py
@@ -1,98 +1,95 @@
-from django.test import SimpleTestCase
-from django.template import Context, Template, TemplateSyntaxError
-from django.conf import settings
-from django.templatetags import static as static_tags
+from django.test import SimpleTestCase, override_settings
+from django.template import Context, Template
 from django.template.base import Variable
+from django.templatetags import static as static_tags
+import urllib.parse
 
-if not settings.configured:
-    settings.configure(STATIC_URL='/static/', MEDIA_URL='/media/')
+class StaticTagExtraTests(SimpleTestCase):
+    def test_get_static_prefix_with_custom_setting(self):
+        with self.settings(STATIC_URL='/assets/'):
+            tpl = Template('{% load static %}{% get_static_prefix %}')
+            rendered = tpl.render(Context())
+            self.assertEqual(rendered, '/assets/')
 
-class StaticTagTests(SimpleTestCase):
-
-    def test_get_static_prefix_direct(self):
-        tpl = Template('{% load static %}{% get_static_prefix %}')
-        rendered = tpl.render(Context())
-        self.assertEqual(rendered, '/static/')
-
-    def test_get_static_prefix_as_var(self):
-        tpl = Template('{% load static %}{% get_static_prefix as sp %}{{ sp }}')
-        rendered = tpl.render(Context())
-        self.assertEqual(rendered, '/static/')
-
-    def test_get_media_prefix_direct(self):
-        with self.settings(MEDIA_URL='/media/'):
+    def test_get_media_prefix_with_custom_setting(self):
+        with self.settings(MEDIA_URL='/uploads/'):
             tpl = Template('{% load static %}{% get_media_prefix %}')
             rendered = tpl.render(Context())
-            self.assertEqual(rendered, '/media/')
+            self.assertEqual(rendered, '/uploads/')
 
-    def test_get_media_prefix_as_var(self):
-        with self.settings(MEDIA_URL='/media/'):
-            tpl = Template('{% load static %}{% get_media_prefix as mp %}{{ mp }}')
-            rendered = tpl.render(Context())
-            self.assertEqual(rendered, '/media/')
+    def test_static_tag_with_empty_string(self):
+        tpl = Template('{% load static %}{% static "" %}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/')
 
-    def test_static_tag_with_string(self):
-        tpl = Template('{% load static %}{% static "img/logo.png" %}')
+    def test_static_tag_with_special_characters(self):
+        tpl = Template('{% load static %}{% static "img/lo go@.png" %}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/img/lo%20go%40.png')
+
+
+    def test_static_tag_with_leading_slash(self):
+        tpl = Template('{% load static %}{% static "/img/logo.png" %}')
         rendered = tpl.render(Context())
         self.assertEqual(rendered, '/static/img/logo.png')
 
-    def test_static_tag_with_variable(self):
+    def test_static_function_with_leading_slash(self):
+        url = static_tags.static('/img/logo.png')
+        self.assertEqual(url, '/static/img/logo.png')
+
+    def test_static_tag_with_context_variable_and_leading_slash(self):
         tpl = Template('{% load static %}{% static path %}')
+        rendered = tpl.render(Context({'path': '/img/logo.png'}))
+        self.assertEqual(rendered, '/static/img/logo.png')
+
+    def test_static_tag_with_unicode_path(self):
+        path = 'img/логотип.png'
+        tpl = Template(f'{{% load static %}}{{% static "{path}" %}}')
+        rendered = tpl.render(Context())
+        expected = '/static/' + urllib.parse.quote(path)
+        self.assertEqual(rendered, expected)
+
+    def test_static_tag_with_none_variable(self):
+        tpl = Template('{% load static %}{% static path %}')
+        rendered = tpl.render(Context({'path': None}))
+        self.assertEqual(rendered, '/static/')
+
+    def test_static_tag_with_integer_variable(self):
+        tpl = Template('{% load static %}{% static path %}')
+        rendered = tpl.render(Context({'path': 123}))
+        self.assertEqual(rendered, '/static/123')
+
+    def test_static_tag_with_nested_variable(self):
+        tpl = Template('{% load static %}{% static obj.path %}')
+        rendered = tpl.render(Context({'obj': {'path': 'img/logo.png'}}))
+        self.assertEqual(rendered, '/static/img/logo.png')
+
+    def test_static_tag_with_as_var_and_variable(self):
+        tpl = Template('{% load static %}{% static path as logo %}{{ logo }}')
         rendered = tpl.render(Context({'path': 'img/logo.png'}))
         self.assertEqual(rendered, '/static/img/logo.png')
 
-    def test_static_tag_as_var(self):
-        tpl = Template('{% load static %}{% static "img/logo.png" as logo %}{{ logo }}')
+    def test_staticnode_repr_with_path_variable(self):
+        node = static_tags.StaticNode(varname=None, path=Variable('"img/logo.png"'))
+        self.assertTrue("StaticNode" in repr(node))
+
+    def test_prefixnode_repr_with_media_url(self):
+        node = static_tags.PrefixNode(varname='foo', name='MEDIA_URL')
+        self.assertEqual(repr(node), "<PrefixNode for 'MEDIA_URL'>")
+
+    def test_bootstrap_tooltip_script_rendered(self):
+        script = """
+        <script>
+        (function () {
+          'use strict'
+          var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+          tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl)
+          })
+        })()
+        </script>
+        """
+        tpl = Template(script)
         rendered = tpl.render(Context())
-        self.assertEqual(rendered, '/static/img/logo.png')
-
-    def test_static_function(self):
-        url = static_tags.static('img/logo.png')
-        self.assertEqual(url, '/static/img/logo.png')
-
-    def test_static_tag_missing_argument(self):
-        tpl = '{% load static %}{% static %}'
-        with self.assertRaises(TemplateSyntaxError):
-            Template(tpl)
-
-    def test_prefixnode_repr(self):
-        node = static_tags.PrefixNode(varname='foo', name='STATIC_URL')
-        self.assertEqual(repr(node), "<PrefixNode for 'STATIC_URL'>")
-
-    def test_staticnode_repr(self):
-        node = static_tags.StaticNode(varname='foo', path=Variable('"img/logo.png"'))
-        self.assertIn("StaticNode", repr(node))
-
-    def test_staticnode_render_autoescape(self):
-        tpl = Template('{% load static %}{% static "<img>.png" %}')
-        rendered = tpl.render(Context(autoescape=True))
-        self.assertNotIn('&lt;img&gt;.png', rendered)
-
-    def test_staticnode_handle_simple_without_staticfiles(self):
-        original_is_installed = static_tags.apps.is_installed
-        static_tags.apps.is_installed = lambda app: False
-        try:
-            url = static_tags.StaticNode.handle_simple('img/logo.png')
-            self.assertEqual(url, '/static/img/logo.png')
-        finally:
-            static_tags.apps.is_installed = original_is_installed
-            
-            def test_tooltip_function_runs_without_error(self):
-                try:
-                    pass
-                except Exception as e:
-                    self.fail(f"Tooltip JS function raised an exception: {e}")
-
-            def test_tooltip_function_document_query_selector_all(self):
-                elements = ['el1', 'el2', 'el3']
-                tooltipTriggerList = list(elements)
-                self.assertEqual(len(tooltipTriggerList), 3)
-
-            def test_tooltip_function_forEach(self):
-                elements = ['el1', 'el2']
-                called = []
-                def fake_tooltip(el):
-                    called.append(el)
-                for el in elements:
-                    fake_tooltip(el)
-                self.assertEqual(called, elements)
+        self.assertIn('bootstrap.Tooltip', rendered)
+        self.assertIn('[data-bs-toggle="tooltip"]', rendered)

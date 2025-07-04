45aa772e73b5cd7ecea263498dcb97aecc365982
Shon-Kalangutkar <shon.kalangutkar@thinknyx.com>
2025-07-04 16:20:36 +0530

    Added Testcases of actionRedirect.js

---
+++ dashboard/test_static.py
@@ -0,0 +1,80 @@
+from django.test import SimpleTestCase
+from django.template import Context, Template, TemplateSyntaxError
+from django.conf import settings
+from django.templatetags import static as static_tags
+from django.template.base import Variable
+
+# Ensure STATIC_URL and MEDIA_URL are set for tests
+if not settings.configured:
+    settings.configure(STATIC_URL='/static/', MEDIA_URL='/media/')
+
+class StaticTagTests(SimpleTestCase):
+
+    def test_get_static_prefix_direct(self):
+        tpl = Template('{% load static %}{% get_static_prefix %}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/')
+
+    def test_get_static_prefix_as_var(self):
+        tpl = Template('{% load static %}{% get_static_prefix as sp %}{{ sp }}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/')
+
+    def test_get_media_prefix_direct(self):
+        with self.settings(MEDIA_URL='/media/'):
+            tpl = Template('{% load static %}{% get_media_prefix %}')
+            rendered = tpl.render(Context())
+            self.assertEqual(rendered, '/media/')
+
+    def test_get_media_prefix_as_var(self):
+        with self.settings(MEDIA_URL='/media/'):
+            tpl = Template('{% load static %}{% get_media_prefix as mp %}{{ mp }}')
+            rendered = tpl.render(Context())
+            self.assertEqual(rendered, '/media/')
+
+    def test_static_tag_with_string(self):
+        tpl = Template('{% load static %}{% static "img/logo.png" %}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/img/logo.png')
+
+    def test_static_tag_with_variable(self):
+        tpl = Template('{% load static %}{% static path %}')
+        rendered = tpl.render(Context({'path': 'img/logo.png'}))
+        self.assertEqual(rendered, '/static/img/logo.png')
+
+    def test_static_tag_as_var(self):
+        tpl = Template('{% load static %}{% static "img/logo.png" as logo %}{{ logo }}')
+        rendered = tpl.render(Context())
+        self.assertEqual(rendered, '/static/img/logo.png')
+
+    def test_static_function(self):
+        url = static_tags.static('img/logo.png')
+        self.assertEqual(url, '/static/img/logo.png')
+
+    def test_static_tag_missing_argument(self):
+        tpl = '{% load static %}{% static %}'
+        with self.assertRaises(TemplateSyntaxError):
+            Template(tpl)
+
+    def test_prefixnode_repr(self):
+        node = static_tags.PrefixNode(varname='foo', name='STATIC_URL')
+        self.assertEqual(repr(node), "<PrefixNode for 'STATIC_URL'>")
+
+    def test_staticnode_repr(self):
+        node = static_tags.StaticNode(varname='foo', path=Variable('"img/logo.png"'))
+        self.assertIn("StaticNode", repr(node))
+
+    def test_staticnode_render_autoescape(self):
+        tpl = Template('{% load static %}{% static "<img>.png" %}')
+        rendered = tpl.render(Context(autoescape=True))
+        self.assertNotIn('&lt;img&gt;.png', rendered)  # Should not escape URL
+
+    def test_staticnode_handle_simple_without_staticfiles(self):
+        # Patch apps.is_installed to return False
+        original_is_installed = static_tags.apps.is_installed
+        static_tags.apps.is_installed = lambda app: False
+        try:
+            url = static_tags.StaticNode.handle_simple('img/logo.png')
+            self.assertEqual(url, '/static/img/logo.png')
+        finally:
+            static_tags.apps.is_installed = original_is_installed
